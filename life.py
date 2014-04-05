2ab36ea3282a50ed5b652f13dff4b684831f5670
boatx <lodka@localhost.localdomain>
2014-04-06 01:13:58 +0200

    Refactoring cd.

--- life.py
+++ life.py
@@ -2,155 +2,169 @@ import sys
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
+NUM_OF_CELLS_X = 36
+NUM_OF_CELLS_Y = 36
+CELL_SIZE = 10
+
+WINDOW_X_SIZE = 450
+WINDOW_Y_SIZE = 480
+
+
 class Cell(QWidget):
+
+    @staticmethod
+    def _gen_matrix(x, y):
+        return [[0 for _ in range(x)]
+                for _ in range(y)]
+
+    @staticmethod
+    def _copy_matrix(matrix):
+        return [list(i) for i in matrix]
 
     def __init__(self):
         QWidget.__init__(self)
 
         self.speed = 1000
-        self.run=False
-        self.table=[ [ 0 for i in range(36) ] for j in range(36) ]
+        #2 rows invisible
+        self.num_of_cells_x = NUM_OF_CELLS_X
+        #2 rows invisible
+        self.num_of_cells_y = NUM_OF_CELLS_Y
+        self.cell_size = CELL_SIZE
+        self.table = self._gen_matrix(self.num_of_cells_x,
+                                      self.num_of_cells_y)
+        self.run = False
         self.setWindowTitle('Automat komorkowy')
         self.setToolTip('Zaznacz komorki')
-        self.stbn=QPushButton('Start',self)
-        self.stbn.setToolTip('Start Symulacji')
-        self.clbn=QPushButton('Wyczysc',self)
-        self.clbn.setToolTip('Wyczysc plansze')
-        self.connect(self.stbn, SIGNAL("clicked()"),self.start)
-        self.connect(self.clbn, SIGNAL("clicked()"),self.clean)
+        self.start_btn = QPushButton('Start', self)
+        self.start_btn.setToolTip('Start Symulacji')
+        self.clear_btn = QPushButton('Wyczysc', self)
+        self.clear_btn.setToolTip('Wyczysc plansze')
+        self.connect(self.start_btn, SIGNAL("clicked()"), self.start)
+        self.connect(self.clear_btn, SIGNAL("clicked()"), self.clean)
         self.timer = QTimer(self)
-        self.connect(self.timer, SIGNAL("timeout()"),self.paintUpdate)
-        self.slider = QSlider(Qt.Horizontal,self)
+        self.connect(self.timer, SIGNAL("timeout()"), self.paint_update)
+        self.slider = QSlider(Qt.Horizontal, self)
         self.slider.setMinimum(5)
         self.slider.setMaximum(1000)
         self.slider.setToolTip('Predkosc symulacji')
         self.slider.setValue(500)
-        self.connect(self.slider, SIGNAL('valueChanged(int)'),self.setValue)
-        buttonLayout=QHBoxLayout()
-        buttonLayout.addWidget(self.stbn)
-        buttonLayout.addWidget(self.clbn)
+        self.connect(self.slider, SIGNAL('valueChanged(int)'), self.set_value)
+        btn_layout = QHBoxLayout()
+        btn_layout.addWidget(self.start_btn)
+        btn_layout.addWidget(self.clear_btn)
         layout = QVBoxLayout()
         layout.setAlignment(Qt.AlignBottom)
-        layout.addLayout(buttonLayout,Qt.AlignCenter)
-        layout.addWidget(self.slider,Qt.AlignCenter)
+        layout.addLayout(btn_layout, Qt.AlignCenter)
+        layout.addWidget(self.slider, Qt.AlignCenter)
         self.setLayout(layout)
-        self.resize(450,480)
 
-    def setValue(self):
+        self.window_x_size = WINDOW_X_SIZE
+        self.window_y_size = WINDOW_Y_SIZE
+        self.resize(self.window_x_size, self.window_y_size)
+
+    def set_value(self):
         self.speed = self.slider.value()
-        #print self.speed
-        if self.run == True:
+        if self.run:
             self.timer.stop()
             self.timer.start(self.speed)
 
     def start(self):
-        if self.run == False:
+        if not self.run:
             self.run = True
-            self.stbn.setToolTip('Koniec Symulacji')
-            self.stbn.setText('Koniec')
+            self.start_btn.setToolTip('Koniec Symulacji')
+            self.start_btn.setText('Koniec')
             self.timer.start(self.speed)
         else:
             self.run = False
-            self.stbn.setToolTip('Start')
-            self.stbn.setText('Start Symulacji')
+            self.start_btn.setToolTip('Start')
+            self.start_btn.setText('Start Symulacji')
             self.timer.stop()
 
     def clean(self):
-        for i in range(36):
-            for j in range(36):
-                self.table[i][j]=0
+        self.table = self._gen_matrix(self.num_of_cells_x,
+                                      self.num_of_cells_y)
         self.repaint()
 
     def mousePressEvent(self, event):
-        x=(event.x()-50)/10
-        y=(event.y()-50)/10
-        if (x<=34 and x>=1) and (y<=34 and y>=1):
-            if self.table[y][x]==1:
-                self.table[y][x]=0
+
+        x = (event.x()-50)/10
+        y = (event.y()-50)/10
+
+        if 1 <= x <= 34 and 1 <= y <= 34:
+            if self.table[y][x] == 1:
+                self.table[y][x] = 0
             else:
-                self.table[y][x]=1
+                self.table[y][x] = 1
             self.repaint()
 
     def mouseMoveEvent(self, event):
-        x=(event.x()-50)/10
-        y=(event.y()-50)/10
-        if (x<=34 and x>=1) and (y<=34 and y>=1):
-            if self.table[y][x]==1:
-                self.table[y][x]=0
-            else:
-                self.table[y][x]=1
-            self.repaint()
 
+        x = (event.x()-50)/10
+        y = (event.y()-50)/10
+
+        if 1 <= x <= 34 and 1 <= y <= 34:
+            if self.table[y][x] == 1:
+                self.table[y][x] = 0
+            else:
+                self.table[y][x] = 1
+            self.repaint()
 
     def paintEvent(self, event):
         painter = QPainter()
         painter.begin(self)
         painter.fillRect(event.rect(), QBrush(Qt.white))
         painter.setRenderHint(QPainter.Antialiasing)
+        for i in range(self.num_of_cells_x):
+            a = i*self.cell_size+50
+            for j in range(self.num_of_cells_y):
+                b = j*self.cell_size+50
+                if self.table[j][i] == 1:
+                    painter.fillRect(a, b, self.cell_size, self.cell_size, QBrush(Qt.blue))
+
+        #rysowanie siatki linii
         painter.setPen(QPen(QBrush(Qt.gray), 1, Qt.SolidLine))
-        for i in range(36):
-            a=i*10+50
-            for j in range(36):
-                b=j*10+50
-                if self.table[j][i]==1:
-                    painter.fillRect(a,b,10,10,QBrush(Qt.blue))
-                #rysowanie siatki linii
-        for i in range(60,410,10):
-            painter.drawLine(60,i,400,i)
-        for i in range(60,410,10):
-            painter.drawLine(i,60,i,400)
+        for i in range(60, 410, 10):
+            painter.drawLine(60, i, 400, i)
+        for i in range(60, 410, 10):
+            painter.drawLine(i, 60, i, 400)
         painter.end()
 
-    def sand(self):
-        for k in range(34,-1,-1):
-            for l in range(34,-1,-1):
-                if self.table[l][k] == 1:
-                    if self.table[l+1][k] == 0:
-                        self.table[l][k] =  0;
-                        self.table[l+1][k] = 1;
+    def paint_update(self):
+        tmp = self._copy_matrix(self.table)
 
-    def paintUpdate(self):
-        tmp=[ [ 0 for i in range(36) ] for j in range(36) ]
-        for i in range(36):
-            for j in range(36):
-                tmp[i][j]=self.table[i][j]
-
-        for j in range(1,35):
-            for i in range(1,35):
-                cnt=0
+        for j in range(1, 35):
+            for i in range(1, 35):
+                counter = 0
                 if self.table[i+1][j+1] == 1:
-                    cnt=cnt+1
+                    counter += 1
                 if self.table[i-1][j-1] == 1:
-                    cnt=cnt+1
+                    counter += 1
                 if self.table[i][j-1] == 1:
-                    cnt=cnt+1
+                    counter += 1
                 if self.table[i-1][j] == 1:
-                    cnt=cnt+1
+                    counter += 1
                 if self.table[i][j+1] == 1:
-                    cnt=cnt+1
+                    counter += 1
                 if self.table[i+1][j] == 1:
-                    cnt=cnt+1
+                    counter += 1
                 if self.table[i-1][j+1] == 1:
-                    cnt=cnt+1
+                    counter += 1
                 if self.table[i+1][j-1] == 1:
-                    cnt=cnt+1
+                    counter += 1
 
                 if self.table[i][j] == 1:
-                    if cnt < 2 or cnt>3:
-                        tmp[i][j]=0
+                    if counter < 2 or counter > 3:
+                        tmp[i][j] = 0
                 else:
-                    if cnt == 3:
-                        tmp[i][j]=1
+                    if counter == 3:
+                        tmp[i][j] = 1
 
-        for i in range(36):
-            for j in range(36):
-                self.table[i][j]=tmp[i][j]
+        self.table = tmp
+
         self.repaint()
 
 
-
 if __name__ == "__main__":
-
     app = QApplication(sys.argv)
     life = Cell()
     life.show()
