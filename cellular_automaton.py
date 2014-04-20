415136ba34ad1328d9c85debb2685f011ca7d45b
boatx <lodka@tlen.pl>
2014-04-21 00:09:37 +0200

    Refactoring + add documentation

--- cellular_automaton.py
+++ cellular_automaton.py
@@ -22,37 +22,86 @@ class CellularAutomatonBaseClass(object):
         self.states_colors = states_colors
 
     def check_cell(self, x, y):
+        """
+        Check value of cell (x,y).
+        :param int x: x coordinate of cell
+        :param int y: y coordinate of cell
+        """
         raise NotImplementedError
 
     def update_cell(self, x, y, state=None):
+        """
+        Update state of cell x,y.
+        :param int x: x coordinate of cell
+        :param int y: y coordinate of cell
+        :param state: state of cell
+        """
         raise NotImplementedError
 
     def update_table(self):
+        """
+        Update values of cell table
+        """
         raise NotImplementedError
 
     def clean(self):
+        """
+        Clean cell table.
+        """
         self.table = self._gen_matrix(self.total_size_x, self.total_size_y)
 
-    def von_neumann_neighborhood(self, i, j, r=1):
+    def von_neumann_neighborhood(self, x, y, r=1):
+        """
+        Return Von Neumann neighborhood of cell x, y.
+
+        :param int x: cell coordinate
+        :param int y: cell coordinate
+        :rtype: list
+        """
         raise NotImplementedError
 
-    def moore_neighborhood(self, i, j):
-        return [row[j-1:j+2] for row in self.table[i-1:i+2]]
+    def moore_neighborhood(self, x, y):
+        """
+        Return Moore neighborhood of cell x, y.
+
+        :param int x: cell coordinate
+        :param int y: cell coordinate
+        :rtype: list
+        """
+        return [row[y-1:y+2] for row in self.table[x-1:x+2]]
 
     @staticmethod
-    def _gen_random_matrix(x, y, values):
+    def _gen_random_matrix(x_len, y_len, values):
+        """
+        Generate table of size x_len y_len with random values.
+
+        :param int x_len: length of first dimension of table
+        :param int y_len: length of second dimension of table
+        :param list values: list of values to set randomly
+        """
         random.seed()
         end = len(values) - 1
-        return [[values[random.randint(0, end)] for _ in range(x)]
-                for _ in range(y)]
+        return [[values[random.randint(0, end)] for _ in range(x_len)]
+                for _ in range(y_len)]
 
     @staticmethod
-    def _gen_matrix(x, y, value=0):
-        return [[value for _ in range(x)]
-                for _ in range(y)]
+    def _gen_matrix(x_len, y_len, value=0):
+        """
+        Generate table of size x_len x y_len and set values of cells to value.
+
+        :param int x_len: length of first dimension of table
+        :param int y_len: length of second dimension of table
+        :param values:
+        """
+        return [[value for _ in range(x_len)]
+                for _ in range(y_len)]
 
     @staticmethod
     def _copy_matrix(matrix):
+        """
+        Return copy of matrix.
+        :param list matrix: matrix to copy
+        """
         return [list(i) for i in matrix]
 
 
@@ -75,17 +124,22 @@ class ConwayLifeOutflow(CellularAutomatonBaseClass):
                                              self.total_size_y,
                                              self.states.values())
 
-    def _neumann_neighborhood_counter(self, i, j, r=1):
+    def _neumann_neighborhood_counter(self, x, y, r=1):
         pass
 
-    def _moore_neighborhood_counter(self, i, j):
+    def _moore_neighborhood_counter(self, x, y):
+        """
+        Count neighbors of cell x,y in Moore Neighborhood.
+        :param int x: x coordinate of cell
+        :param int y: y coordinate of cell
+        """
         counter = 0
-        for row in self.moore_neighborhood(i, j):
-            for x in row:
-                if x:
+        for row in self.moore_neighborhood(x, y):
+            for item in row:
+                if item:
                     counter += 1
 
-        if self.table[i][j]:
+        if self.table[x][y]:
             counter -= 1
 
         return counter
@@ -125,4 +179,8 @@ class ConwayLifeOutflow(CellularAutomatonBaseClass):
 
 
 class Ant(CellularAutomatonBaseClass):
+    pass
+
+
+class Sand(CellularAutomatonBaseClass):
     pass
